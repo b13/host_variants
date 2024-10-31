dbe17652ac7bd08e3683b486f2bfb176724cae4c
Alexander Schnitzler <git@alexanderschnitzler.de>
2024-10-31 09:18:23 +0100

    [TASK] Allow installation with TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-12.9.99'
+            'typo3' => '9.5.0-13.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
